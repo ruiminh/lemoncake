96cd8a290ceb0ffc7ae276849821b478bd50fff1
Ruimin Huang <cph-rh206@cphbusiness.dk>
2021-04-29 08:06:16 +0200

    code insertorder,has bug

--- src/main/java/business/persistence/CakeMapper.java
+++ src/main/java/business/persistence/CakeMapper.java
@@ -1,13 +1,11 @@
 package business.persistence;
 
+import business.entities.BasketEntry;
 import business.entities.Bottom;
 import business.entities.Topping;
 import business.exceptions.UserException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,4 +56,67 @@ public class CakeMapper {
         }
         return BottomList;
     }
+    public void insertOrder(int user_id, int price, List<BasketEntry> cupcakeList)throws UserException {
+        try (Connection connection = database.connect())
+        {
+            String sql = "INSERT INTO order (user_id,price) VALUES (?,?);";
+
+
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+            {
+                ps.setInt(1, user_id);
+                ps.setInt(2, price);
+                ps.executeUpdate();
+                ResultSet ids = ps.getGeneratedKeys();
+                ids.next();
+                int orderId = ids.getInt(1);
+
+                //todo: should insert hobby to link_hobby_bmi
+                for (BasketEntry basketEntry : cupcakeList) {
+                    insertBasketEntry(orderId,basketEntry);
+
+                }
+
+            }
+            catch (SQLException ex)
+            {
+                throw new UserException(ex.getMessage());
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new UserException(ex.getMessage());
+        }
+
+    }
+
+    private void insertBasketEntry(int orderId, BasketEntry basketEntry) throws UserException
+    {
+        try (Connection connection = database.connect())
+        {
+            int amount = basketEntry.getAmount();
+            int bottomId = basketEntry.getBottom().getBottomId();
+            int toppingId = basketEntry.getTopping().getToppingId();
+            String sql = "INSERT INTO cupcake_entry (order_id,number,bottom_id,topping_id)  VALUES (?,?,?,?)";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ps.setInt(1, orderId);
+                ps.setInt(2, amount);
+                ps.setInt(3, bottomId);
+                ps.setInt(4, toppingId);
+                ps.executeUpdate();
+            }
+            catch (SQLException ex)
+            {
+                throw new UserException(ex.getMessage());
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new UserException(ex.getMessage());
+        }
+
+    }
+
 }
--- src/main/java/web/commands/CheckOutCommand.java
+++ src/main/java/web/commands/CheckOutCommand.java
@@ -3,6 +3,7 @@ package web.commands;
 import business.entities.Basket;
 import business.entities.User;
 import business.exceptions.UserException;
+import business.persistence.CakeMapper;
 import business.services.UserFacade;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,12 +19,14 @@ public class CheckOutCommand extends CommandProtectedPage
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
         UserFacade userFacade = new UserFacade(database);
+        CakeMapper cakeMapper = new CakeMapper(database);
         HttpSession session = request.getSession();
         User user = (User)session.getAttribute("user");
         int id = user.getId();
         Basket basket =(Basket)session.getAttribute("basket");
         int newBalance =user.getBalance()-basket.totalPrice();
         userFacade.updateUserBalance(id,newBalance);
+        cakeMapper.insertOrder(id,basket.totalPrice(),basket.getCupcakeList());
         return pageToShow;
     }
 }
